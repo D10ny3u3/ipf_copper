3519516690e8465121577183065123f96307395d
D10ny3u3 <yankai19911130@gmail.com>
2026-08-06 09:59:37 +0800

    Implement geneset plotting for copper ion response
    Added plotting functionality for genesets related to copper ion response and transport, including saving plots as PDF and SVG.

--- plot_related/plot_genestes.R
+++ plot_related/plot_genestes.R
@@ -35,4 +35,58 @@ for(i in seq_along(original_names)){
   hab_epi[[old_name]] <- NULL
 }
 
+library(ggplot2)
+
+sci_colors <- c(
+  "#a1a9d0", "#f0988c", "#b883d3", "#9e9e9e",
+  "#cfeaf1", "#c4a5de", "#f6cae5", "#96cccb"
+)
+
+plots <- lapply(
+  clean_names,
+  function(x){
+    plot_geneset_pseudobulk_box(
+      seu_obj = hab_epi,
+      geneset_name = x
+    )
+  }
+)
+
+library(patchwork)
+combined_plot <- wrap_plots(plots, ncol = 4) 
+print(combined_plot)
+ggsave(
+  filename = "hab_epi_copper.pdf", 
+  plot = combined_plot,          
+  width = 20,                      
+  height = 10,                   
+  dpi = 300                      
+)
+
+epi_response_copper <- 
+  plot_geneset_pseudobulk_box(
+    seu_obj = hab_epi, 
+    "GOBP_CELLULAR_RESPONSE_TO_COPPER_ION",
+    y_lab = "CELL_RESP_COPPER_ION")
+
+ggsave(
+  "epi_response_copper.svg",
+  epi_response_copper,
+  width=2,
+  height=4
+)
+
+epi_transport <- 
+  plot_geneset_pseudobulk_box(
+    seu_obj = hab_epi, 
+    "GOBP_COPPER_ION_TRANSPORT",
+    y_lab = "COPPER_ION_TRANSPORT")
+
+ggsave(
+  "epi_transport.svg",
+  epi_transport,
+  width=2,
+  height=4
+)
+
 
